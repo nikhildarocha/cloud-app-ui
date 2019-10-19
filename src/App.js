5ccca7ee391b5024ded58c9b5101800ea872edb5
nikhildarocha <niks77883@gmail.com>
2019-10-20 02:52:51 +0530

    ui for cloud app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {message: ''};
+    }
+
+    componentDidMount() {
+        const url = 'http://localhost:8080/';
+        axios.get(url).then(response => {
+            console.log('response : ');
+            console.log(response);
+            this.setState({message: response.data});
+        }).catch(error => {
+            console.log('error : ');
+            console.log(error);
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                Message : {this.state.message}
+            </div>
+        );
+    }
 }
 
 export default App;
